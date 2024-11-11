447664f6fdf21a4270a04df6f6c9aeda1a35a7cb
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-11-11 16:26:33 +0200

    connect to supabase, fetch and update song data from discord embed

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-import { REST, Routes, Client, Events, GatewayIntentBits } from "discord.js";
+import {
+    // REST, Routes,
+    Client,
+    Events,
+    GatewayIntentBits,
+} from "discord.js";
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASEURL = Bun.env.SUPA_URL;
@@ -12,16 +17,17 @@ if (!CLIENT_ID) throw Error("No discord client_id");
 if (!SUPABASEKEY) throw Error("No supabasekey");
 
 const supabase = createClient(SUPABASEURL, SUPABASEKEY);
-const client = new Client({
+
+const discordClient = new Client({
     intents: [GatewayIntentBits.Guilds, "GuildMessages", "MessageContent"],
 });
 
-client.once(Events.ClientReady, (readyClient) => {
+discordClient.once(Events.ClientReady, (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on("messageCreate", (message) => {
-    console.log("ali");
+discordClient.on("messageCreate", (message) => {
+    // console.log("ali");
 
     if (message.author.bot && message.author.id === "972039350407823400") {
         // console.log(message);
@@ -29,28 +35,68 @@ client.on("messageCreate", (message) => {
     }
 });
 
-client.on("messageUpdate", (_oldMessage, newMessage) => {
-    console.log("ali");
-
+discordClient.on("messageUpdate", async (_oldMessage, newMessage) => {
     if (!newMessage.author) {
         return;
     }
 
+    const embedsTitle = newMessage.embeds[0].title;
+    const embedsUrl = newMessage.embeds[0].url;
+
     if (
         newMessage.author.bot &&
         newMessage.author.id === "972039350407823400" &&
-        newMessage.embeds[0].title &&
-        newMessage.embeds[0].url
+        embedsTitle &&
+        embedsUrl
     ) {
-        console.log(JSON.stringify(newMessage.embeds, null, 4));
+        // console.log(JSON.stringify(newMessage.embeds, null, 4));
+        const { data } = await supabase
+            .from("songs")
+            .select("name, amount")
+            .eq("name", embedsTitle)
+            .eq("url", embedsUrl);
 
-        //there should only be  1 embed
-        for (const data of newMessage.embeds) {
-            console.log(data.title);
-            console.log(data.url);
+        if (data?.length !== 0) {
+            //if song found update its amount
+            const { error } = await supabase
+                .from("songs")
+                // biome-ignore lint/style/noNonNullAssertion: if data.length > 0, data[0] always exists
+                .update({ amount: data![0].amount + 1 })
+                .eq("name", embedsTitle)
+                .eq("url", embedsUrl);
+
+            if (error) {
+                console.log("error in update");
+                console.log(error);
+                return;
+            }
+
+            //settimeout to hopefully prevent duplicate listens
+            setTimeout(() => {}, 1000);
+
+            console.log(
+                // biome-ignore lint/style/noNonNullAssertion: if data.length > 0, data[0] always exists
+                `Updated ${embedsTitle} to ${data![0].amount + 1} listens`,
+            );
+
+            return;
         }
+
+        const { error } = await supabase.from("songs").insert({
+            name: embedsTitle,
+            url: embedsUrl,
+            amount: 1,
+        });
+        //settimeout to hopefully prevent duplicate listens
+        setTimeout(() => {}, 1000);
+
+        if (error) {
+            console.log("error in initializing value");
+            console.log(error);
+            return;
+        }
+        console.log(`Initialized ${embedsTitle}`);
     }
 });
 
-// Log in to Discord with your client's token
-client.login(TOKEN);
+discordClient.login(TOKEN);
